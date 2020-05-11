a2b53698847f04aed489c2d49da90b65c01b6d46
lomocol <lomocol99@gmail.com>
2020-05-11 23:45:21 +0300

    add button section in header

--- Classes/Header.cpp
+++ Classes/Header.cpp
@@ -14,20 +14,21 @@ Header::Header(cocos2d::Node* _parent)
 
 	// add node for health and protection bars
 	barNode = Node::create();
-	barNode->setAnchorPoint(Point(0,0));
+	barNode->setAnchorPoint(Point(0, 0));
 	barNode->setContentSize(barNodeSize);
-	barNode->setPosition(Point(0,0));
-	parent->addChild(barNode,2);
+	barNode->setPosition(Point(0, 0));
+	parent->addChild(barNode, 2);
 
 	// add node for skills buttons
-	//buttonNode = Node::create();
-	//buttonNode->setAnchorPoint(Point(0, 0));
-	//buttonNode->setContentSize(buttonNodeSize);
-	//buttonNode->setPosition(Point(0, 0));
-	//parent->addChild(buttonNode);
+	buttonNode = Node::create();
+	buttonNode->setAnchorPoint(Point(0, 0));
+	buttonNode->setContentSize(buttonNodeSize);
+	buttonNode->setPosition(Point(barNodeSize.width,0));
+	parent->addChild(buttonNode,2);
 
 	setBackground();
 	createBars();
+	createButtons();
 }
 
 void Header::changeHealth(float newHealth)
@@ -52,20 +53,20 @@ Header::~Header()
 void Header::createBars()
 {
 	// calculate bars position
-	Size barNodeSize = barNode->getContentSize();
-	int barHeight = barNodeSize.height * BAR_HEIGHT_PERCENT;
-	int barWidth = barNodeSize.width * BAR_WIDTH_PERCENT;
-	int verticalPadding = (barNodeSize.height - 2 * barHeight) / 3;
-	int horizontalPadding = (barNodeSize.width - barWidth) / 2;
-	Vec2 healthPosition(horizontalPadding, verticalPadding);
-	Vec2 protectionPosition(horizontalPadding, verticalPadding * 2 + barHeight);
+	const Size barNodeSize = barNode->getContentSize();
+	const int barHeight = barNodeSize.height * BAR_HEIGHT_PERCENT;
+	const int barWidth = barNodeSize.width * BAR_WIDTH_PERCENT;
+	const int verticalPadding = (barNodeSize.height - 2 * barHeight) / 3;
+	const int horizontalPadding = (barNodeSize.width - barWidth) / 2;
+	const Vec2 healthPosition(horizontalPadding, verticalPadding);
+	const Vec2 protectionPosition(horizontalPadding, verticalPadding * 2 + barHeight);
 
-	
+
 
 	healthBar = LoadingBar::create("bar.png");
 	// calculate scale value to resize bar.png
-	Size rawBarSize = healthBar->getContentSize();
-	Vec2 scaleVal(barWidth / rawBarSize.width , barHeight / rawBarSize.height);
+	const Size rawBarSize = healthBar->getContentSize();
+	const Vec2 scaleVal(barWidth / rawBarSize.width, barHeight / rawBarSize.height);
 
 	healthBar->setColor(Color3B::RED);
 	healthBar->setDirection(LoadingBar::Direction::LEFT);
@@ -85,7 +86,22 @@ void Header::createBars()
 
 void Header::createButtons()
 {
-	
+	// calculate buttons position
+	const Size buttonNodeSize = buttonNode->getContentSize();
+	const int buttonWidth = buttonNodeSize.width * buttonWidthPercent;
+	const int buttonHeight = buttonNodeSize.height * buttonHeightPercent;
+	const int horizontalPadding = (buttonNodeSize.width - 3 * buttonWidth) / 4;
+	const int verticalCenter = buttonNodeSize.height / 2;
+	const Vec2 firstButtonPosition(horizontalPadding, verticalCenter);
+	const Vec2 secondButtonPosition(firstButtonPosition.x + horizontalPadding + buttonWidth, verticalCenter);
+	const Vec2 thirdButtonPosition(secondButtonPosition.x + horizontalPadding + buttonWidth, verticalCenter);
+	const Size buttonSize(buttonWidth, buttonHeight);
+
+
+	addButton(shieldButton, shieldButtonFileName, shieldButtonUnavailableFileName, firstButtonPosition, buttonSize);
+	addButton(fireBallButton, fireBallButtonFileName, fireBallButtonUnavailableFileName, secondButtonPosition, buttonSize);
+	addButton(blockButton, blockButtonFileName, blockButtonUnavailableFileName, thirdButtonPosition, buttonSize);
+
 }
 
 void Header::setBackground()
@@ -95,5 +111,17 @@ void Header::setBackground()
 	background = Sprite::createWithTexture(texture);
 	background->setAnchorPoint(Point(0, 0));
 	background->setPosition(0, 0);
-	parent->addChild(background,1);
+	parent->addChild(background, 1);
+}
+
+void Header::addButton(cocos2d::ui::Button* button, const std::string& normalImageFileName,
+	const std::string& unavailableImageFileName, const cocos2d::Vec2 position,const cocos2d::Size buttonSize)
+{
+	button = Button::create(normalImageFileName, normalImageFileName, unavailableImageFileName);
+	Size rawButtonSize = button->getContentSize();
+	Vec2 scaleValue(buttonSize.width / rawButtonSize.width, buttonSize.height / rawButtonSize.height);
+	button->setScale(scaleValue.x, scaleValue.y);
+	button->setAnchorPoint(Point(0,0.5));
+	button->setPosition(position);
+	buttonNode->addChild(button);
 }
--- Classes/Header.h
+++ Classes/Header.h
@@ -3,6 +3,15 @@
 #include "definitions.h"
 #include "ui/CocosGUI.h"
 
+const static float buttonWidthPercent = 0.2f;
+const static float buttonHeightPercent = 0.5f;
+const static std::string fireBallButtonFileName = "buttons/fireball.png";
+const static std::string fireBallButtonUnavailableFileName = "buttons/fireball_unavailable.png";
+const static std::string blockButtonFileName = "buttons/block.png";
+const static std::string blockButtonUnavailableFileName = "buttons/block_unavailable.png";
+const static std::string shieldButtonFileName = "buttons/shield.png";
+const static std::string shieldButtonUnavailableFileName = "buttons/shield_unavailable.png";
+
 class Header
 {
 public:
@@ -12,7 +21,10 @@ public:
 	~Header() ;
 private:
 	void createBars();
+	void createButtons();
 	void setBackground();
+	void addButton(cocos2d::ui::Button*  button,const std::string & normalImageFileName,
+		const std::string& unavailableImageFileName,const cocos2d::Vec2 position, const cocos2d::Size buttonSize);
 private:
 	cocos2d::Sprite* background;
 	cocos2d::Size headerSize;
@@ -21,5 +33,9 @@ private:
 	cocos2d::Node* barNode;
 	cocos2d::ui::LoadingBar* healthBar;
 	cocos2d::ui::LoadingBar* protectionBar;
+
+	cocos2d::ui::Button* fireBallButton;
+	cocos2d::ui::Button* shieldButton;
+	cocos2d::ui::Button* blockButton;
 };
 
